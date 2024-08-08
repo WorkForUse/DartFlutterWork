f646f36413b38f9d37836feb32ac38366db4218e
Muhammad Hamza <149518339+WorkForUse@users.noreply.github.com>
2024-08-08 03:52:20 -0700

    Create LoopsWorks.dart

---
+++ bin/LoopsWorks.dart
@@ -0,0 +1,66 @@
+// Leep Year
+
+// void main() {
+//   int startYear = 2001;
+//   int endYear = DateTime.now().year;
+
+//   for (int year = startYear; year <= endYear; year++) {
+//     if (isLeapYear(year)) {
+//       print('$year is a leap year.');
+//     }
+//   }
+// }
+
+// bool isLeapYear(int year) {
+//   if (year % 4 == 0) {
+//     if (year % 100 == 0) {
+//       if (year % 400 == 0) {
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     } else {
+//       return true;
+//     }
+//   } else {
+//     return false;
+//   }
+// }
+
+// void main() {
+//   int number = 5; // You can change this number to any positive integer
+//   int factorial = 1;
+
+//   for (int i = 1; i <= number; i++) {
+//     factorial *= i;
+//   }
+
+//   print('Factorial of $number is $factorial');
+// }
+
+// void main() {
+//   int number = 5; // You can change this number to any positive integer
+//   int factorial = 1;
+
+//   for (int i = 1; i <= number; i++) {
+//     factorial *= i;
+//   }
+
+//   print('Factorial of $number is $factorial');
+// }
+
+// void main() {
+//   for (int i = 1; i <= 100; i++) {
+//     if (i == 22 || i == 24 || i == 26 || i == 28 || i == 30) {
+//       continue; // Skip these numbers
+//     }
+//     if (i > 50) {
+//       break; // Stop the loop at 50
+//     }
+//     if (i % 2 == 0) {
+//       print('$i is even');
+//     } else {
+//       print('$i is odd');
+//     }
+//   }
+// }
